935f6ede0920ecc858e357a2fc7f3a122e825429
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2020-11-23 03:11:59 -0300

    Simplified React class example

--- example/src/UI/FromComponent.js
+++ example/src/UI/FromComponent.js
@@ -1,17 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
 
 import {Subscription} from 'react-state-events'
 
 export default ({stateEvents})=>{
-    const [value, setValue] = useState(1);
-    useEffect(() => {
-        stateEvents.subscribe(
-            (data)=>setValue(data),
-            ()=>{} //ignore stream errors
-        );
-    },[]);
     const onClick = ()=>{
-        stateEvents.publish(value+1);
+        stateEvents.publish(stateEvents.getCurrent()+1);
     }
     const onError = (err)=>{
         console.log('FromComponent handled error:',err);
