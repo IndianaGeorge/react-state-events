b4665d9f84192945c6c58a572d1f5df0478a39b4
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2023-02-14 00:56:02 -0300

    Updates

---
+++ precache-manifest.74bacc52fe3fe6539b8f401af1e666c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a55c975bb3652c2e73a08abda706e13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abeeef9e6fbb7f5b61c2",
+    "url": "./static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "98e7be5283a82f9035c2",
+    "url": "./static/js/2.f5f17c0f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "./static/js/2.f5f17c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abeeef9e6fbb7f5b61c2",
+    "url": "./static/js/main.7daaac70.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.7daaac70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1b7ac9aa614dd437c8",
+    "url": "./static/js/runtime-main.586b2c7e.js"
+  }
+]);
