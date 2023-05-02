54f41c1a39b1563f2e9f84abd69ac64413cdd650
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2023-05-01 22:32:48 -0400

    added dev/prod mode indicator to example

--- example/src/App.js
+++ example/src/App.js
@@ -5,41 +5,46 @@ import FromHook from './UI/FromHook';
 import FromComponent from './UI/FromComponent';
 import styles from './App.module.css';
 
-export default (props) => {
+export default () => {
   const counterEvents = new StateEvents(0,"single counter");
   const extCounterEventsA = new ExternalStateEvents(0,"counter");
   const extCounterEventsB = new ExternalStateEvents(0,"counter");
   return (
-    <div className={styles.app}>
+    <div>
+      <span className={styles.mode}>
+        {process.env.NODE_ENV !== 'production'?'development mode':'production mode'}
+      </span>
+      <div className={styles.app}>
 
-      <div className={styles.context}>
-        <h1>StateEvents</h1>
-        <div>
-          <div className={styles.type}>
+        <div className={styles.context}>
+          <h1>StateEvents</h1>
+          <div>
+            <div className={styles.type}>
+              <h2>useStateEvents hook</h2>
+                <FromHook stateEvents={counterEvents} />
+            </div>
+            <div className={styles.type}>
+              <h2>Subscription class</h2>
+              <FromComponent stateEvents={counterEvents} />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.context}>
+          <h1>ExternalStateEvents</h1>
+          <div>
+            <div className={styles.type}>
             <h2>useStateEvents hook</h2>
-              <FromHook stateEvents={counterEvents} />
-          </div>
-          <div className={styles.type}>
+              <FromHook stateEvents={extCounterEventsA} />
+            </div>
+            <div className={styles.type}>
             <h2>Subscription class</h2>
-            <FromComponent stateEvents={counterEvents} />
+              <FromComponent stateEvents={extCounterEventsB} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.context}>
-        <h1>ExternalStateEvents</h1>
-        <div>
-          <div className={styles.type}>
-          <h2>useStateEvents hook</h2>
-            <FromHook stateEvents={extCounterEventsA} />
-          </div>
-          <div className={styles.type}>
-          <h2>Subscription class</h2>
-            <FromComponent stateEvents={extCounterEventsB} />
-          </div>
-        </div>
       </div>
-
     </div>
   )
 }
